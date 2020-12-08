101be9b6e5a47f36b76f8d01ab679ed4936de242
rwaterma <rolf.watermann@coremedia.com>
2020-12-08 15:59:12 +0100

    Do not use CMS Spring XML files in plugins

--- feedback-hub-searchmetrics-lib/src/main/java/com/coremedia/blueprint/searchmetrics/SearchmetricsServiceConfiguration.java
+++ feedback-hub-searchmetrics-lib/src/main/java/com/coremedia/blueprint/searchmetrics/SearchmetricsServiceConfiguration.java
@@ -2,16 +2,12 @@ package com.coremedia.blueprint.searchmetrics;
 
 import com.coremedia.blueprint.searchmetrics.helper.BriefingAssigments;
 import com.coremedia.cache.Cache;
-import com.coremedia.cms.common.plugins.beansforplugins.CommonBeansForPluginsConfiguration;
-import com.coremedia.springframework.xml.ResourceAwareXmlBeanDefinitionReader;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -23,13 +19,6 @@ import java.util.Collections;
  *
  */
 @Configuration
-@ImportResource(
-        value = {
-                "classpath:com/coremedia/cap/common/uapi-services.xml",
-        },
-        reader = ResourceAwareXmlBeanDefinitionReader.class
-)
-@Import({CommonBeansForPluginsConfiguration.class})
 public class SearchmetricsServiceConfiguration {
 
   @Bean
