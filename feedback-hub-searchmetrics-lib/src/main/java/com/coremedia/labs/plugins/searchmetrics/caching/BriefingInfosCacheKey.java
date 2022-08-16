32c7552ab87b289f10ba8e04dfd6869b9dfd9825
Matthias Faust <matthias.faust@coremedia.com>
2022-08-16 10:18:51 +0200

    Better JSON error handling

--- feedback-hub-searchmetrics-lib/src/main/java/com/coremedia/labs/plugins/searchmetrics/caching/BriefingInfosCacheKey.java
+++ feedback-hub-searchmetrics-lib/src/main/java/com/coremedia/labs/plugins/searchmetrics/caching/BriefingInfosCacheKey.java
@@ -1,13 +1,18 @@
 package com.coremedia.labs.plugins.searchmetrics.caching;
 
+import com.coremedia.cache.Cache;
+import com.coremedia.cache.CacheKey;
 import com.coremedia.labs.plugins.searchmetrics.SearchmetricsConnector;
 import com.coremedia.labs.plugins.searchmetrics.SearchmetricsSettings;
 import com.coremedia.labs.plugins.searchmetrics.documents.BriefingInfo;
+import com.coremedia.labs.plugins.searchmetrics.documents.BriefingList;
+import com.coremedia.labs.plugins.searchmetrics.documents.ContentExperience;
+import com.coremedia.labs.plugins.searchmetrics.documents.QueryData;
 import com.coremedia.labs.plugins.searchmetrics.documents.QueryResult;
 import com.coremedia.labs.plugins.searchmetrics.queries.BriefingsQuery;
-import com.coremedia.cache.Cache;
-import com.coremedia.cache.CacheKey;
 import edu.umd.cs.findbugs.annotations.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collections;
 import java.util.List;
@@ -17,6 +22,7 @@ import java.util.Optional;
  *
  */
 public class BriefingInfosCacheKey extends CacheKey<List<BriefingInfo>> {
+  private static Logger LOG = LoggerFactory.getLogger(BriefingInfosCacheKey.class);
 
   private SearchmetricsConnector connector;
   private SearchmetricsSettings settings;
@@ -33,7 +39,16 @@ public class BriefingInfosCacheKey extends CacheKey<List<BriefingInfo>> {
     BriefingsQuery query = new BriefingsQuery(settings.getProjectId());
     Optional<QueryResult> queryResult = connector.postResource(settings, query.toString(), QueryResult.class);
     if (queryResult.isPresent()) {
-      return queryResult.get().getData().getContentExperience().getBriefingsList().getBriefings();
+      QueryData data = queryResult.get().getData();
+      ContentExperience contentExperience = data.getContentExperience();
+      if(contentExperience != null) {
+        BriefingList briefingsList = contentExperience.getBriefingsList();
+        List<BriefingInfo> briefings = briefingsList.getBriefings();
+        LOG.info("Searchmetrics returned " + briefings.size() + " briefings.");
+      }
+      else {
+        LOG.warn("Failed to load briefings from Searchmetrics, view LoggingInterceptor debug logs for details.");
+      }
     }
     return Collections.emptyList();
   }
